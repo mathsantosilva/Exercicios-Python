daabef6d046469569e46a73db6c625d939278a25
Matheus Santos <matheus.santos.x4@gmail.com>
2024-01-09 17:10:20 -0300

    Resume: Finalizando aula 11 - seção 3

---
+++ Introdução/dir_help.py
@@ -0,0 +1,23 @@
+"""
+Seção 3 - Aula 11
+Comandos Dir e Help
+
+-- Utilizarios python para auxiliar na programação
+
+Dir -> Apresenta todos os atributos e funções/metodos para determinado tipo de dado ou variavel.
+Exemplo: dir(**dado ou variavel**)
+
+    Cenario 1: Passando uma string para o dir
+    EX: dir("teste")
+    Result: Ira retornar uma lista de metodos/funções suportadas pelo tipo string
+
+Help -> Apresenta a documentação relacionada a os atributos/propriedades e/ou funções/metodos disponiveis para
+determinado tipo de dado ou variavel.
+Exemplo: help(**função ou metodos que quer obter a documentação**)
+
+    Cenario 1: Passando o metodo "lower" para o help
+    EX: help("teste".lower)
+    Result: Ira trazer a documentação referente ao metodo lower
+
+
+"""
